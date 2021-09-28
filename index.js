5edecf0036cae697bda829e18de62e4225702557
Jacob <yawjacob@yahoo.com>
2021-09-27 19:09:54 -0500

    commit tree c39088e3db57a3b1452a189ead57de135655f7b9

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(blocks) {
+    let distance
+    if (blocks >= 42) {
+    distance = blocks - 42
+        } else {
+            distance = 42 - blocks
+        }
+    return distance
+    }
+
+function distanceFromHqInFeet(blocks) {
+    let distanceInFeet = distanceFromHqInBlocks(blocks) * 264
+    return distanceInFeet
+}
+
+function distanceTravelledInFeet(pointA, pointB) {
+    let distanceTravelled
+    if (pointA >= pointB) {
+        distanceTravelled = (pointA - pointB) * 264
+    } else {
+        distanceTravelled = (pointB - pointA) * 264
+    }
+    return distanceTravelled
+}
+
+function calculatesFarePrice(start, destination) {
+    
+    let fare;
+    if (distanceTravelledInFeet(start, destination) < 400) {
+        fare = 0
+    } else if (distanceTravelledInFeet(start, destination) <= 2000) {
+        fare = (distanceTravelledInFeet(start, destination) - 400) * .02
+    } else if (distanceTravelledInFeet(start, destination) <= 2500) {
+        fare = 25
+    } else if (distanceTravelledInFeet(start, destination) >= 2500) {
+        fare = 'cannot travel that far'
+    }
+    return fare
+}
